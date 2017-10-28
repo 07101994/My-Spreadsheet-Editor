f48b84ae4fb261e280789af52bb27af1e1ca956e
Tushar Koshti <ktk1994@outlook.com>
2017-10-28 13:18:14 +0530

    Project commit
    Full application commit.

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+#region Copyright Syncfusion Inc. 2001 - 2017
+// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using Syncfusion.Windows.Tools.Controls;
+using System.Windows;
+using System.IO;
+using System;
+using Syncfusion.UI.Xaml.Grid.Utility;
+using System.Windows.Media.Imaging;
+using System.Linq;
+using Syncfusion.UI.Xaml.SpreadsheetHelper;
+using Syncfusion.UI.Xaml.Spreadsheet.GraphicCells;
+
+namespace MySpreadsheetEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+		public string FilePath { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+			spreadsheetRibbon.Loaded += SpreadsheetRibbon_Loaded;
+			//For importing charts,
+			this.spreadsheetControl.AddGraphicChartCellRenderer(new GraphicChartCellRenderer());
+		}
+
+		private void SpreadsheetRibbon_Loaded(object sender, RoutedEventArgs e)
+		{
+			var ribbon1 = GridUtil.GetVisualChild<Ribbon>(sender as FrameworkElement);
+
+			if (ribbon1 != null)
+			{
+				RibbonTab ribbonTab = new RibbonTab();
+				ribbonTab.Caption = "OTHER";
+				RibbonButton Button1 = new RibbonButton();
+				Button1.Label = "PRINT";
+				Button1.SmallIcon = new BitmapImage(new Uri("/../Icon/Icons_Print.png", UriKind.Relative));
+				Button1.Click += PrintButton_Click;
+				
+				var customRibbonBar = new RibbonBar();
+				customRibbonBar.Header = "Printing Options";
+				customRibbonBar.Items.Add(Button1);
+				customRibbonBar.IsLauncherButtonVisible = false;
+				ribbonTab.Items.Add(customRibbonBar);
+				ribbon1.Items.Add(ribbonTab);
+			}
+			//var ribbon1 = GridUtil.GetVisualChild<Ribbon>(sender as FrameworkElement);
+
+			//// To add the ribbon button in View tab,
+
+			//if (ribbon1 != null)
+			//{
+			//	var ribbonTab = ribbon1.Items[2] as RibbonTab;
+			//	RibbonButton Button1 = new RibbonButton
+			//	{
+			//		Label = "PRINT",
+			//		SmallIcon = new BitmapImage(new Uri("/../Icon/Icons_Print.png", UriKind.Relative))
+			//	};
+			//	Button1.Click += PrintButton_Click;
+			//	ribbonTab.Items.Add(Button1);
+			//}
+		}
+
+		private void PrintButton_Click(object sender, RoutedEventArgs e)
+		{
+			PrintPreviewWindow previewwindow = new PrintPreviewWindow() { Spreadsheet = spreadsheetControl };
+			previewwindow.ShowDialog();
+		}
+
+		/// <summary>
+		/// Provide support for Excel like closing operation when press the close button.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void RibbonWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.spreadsheetControl.Commands.FileClose.Execute(null);
+            if (Application.Current.ShutdownMode != ShutdownMode.OnExplicitShutdown)
+                e.Cancel = true;
+        }
+    }
+}
---
+++ CS/Printing/Commands/PrintingCommand.cs
@@ -0,0 +1,99 @@
+﻿using Syncfusion.ExcelToPdfConverter;
+using Syncfusion.Pdf;
+using Syncfusion.UI.Xaml.Spreadsheet;
+using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySpreadsheetEditor.Printing.Commands
+{
+	public static class PrintingCommand
+	{
+		static PrintingCommand()
+		{
+			CommandManager.RegisterClassCommandBinding(typeof(SfSpreadsheet), new CommandBinding(PrintPreview, OnExecutePrintPreview, OnCanExecutePrintPreview));
+			CommandManager.RegisterClassCommandBinding(typeof(SfSpreadsheet), new CommandBinding(DirectPrint, OnExecuteDirectPrint, OnCanExecuteDirectPrint));
+		}
+
+
+		#region PrintPreview
+
+		public static RoutedCommand PrintPreview = new RoutedCommand("PrintPreview", typeof(SfSpreadsheet));
+
+		private static void OnExecutePrintPreview(object sender, ExecutedRoutedEventArgs args)
+		{
+			SfSpreadsheet spreadsheetControl = args.Source as SfSpreadsheet;
+			PrintPreviewWindow previewwindow = new PrintPreviewWindow() { Spreadsheet = spreadsheetControl };
+			previewwindow.ShowDialog();
+		}
+
+		private static void OnCanExecutePrintPreview(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = true;
+		}
+
+		#endregion
+
+		#region DirectPrint
+
+		public static RoutedCommand DirectPrint = new RoutedCommand("DirectPrint", typeof(SfSpreadsheet));
+
+		private static void OnExecuteDirectPrint(object sender, ExecutedRoutedEventArgs args)
+		{
+			SfSpreadsheet spreadsheetControl = args.Source as SfSpreadsheet;
+			PrintFromPdfViewer(spreadsheetControl);
+
+		}
+
+		private static void OnCanExecuteDirectPrint(object sender, CanExecuteRoutedEventArgs args)
+		{
+			args.CanExecute = true;
+		}
+
+		#endregion
+
+		#region Direct print through PdfViewer
+
+		private static void PrintFromPdfViewer(SfSpreadsheet spreadsheetControl)
+		{
+			//Create the pdfviewer for load the document.
+			PdfViewerControl pdfviewer = new PdfViewerControl();
+
+			// PdfDocumentViewer
+			MemoryStream pdfstream = new MemoryStream();
+
+			ExcelToPdfConverter converter = new ExcelToPdfConverter(spreadsheetControl.Workbook);
+			//Intialize the PdfDocument
+			PdfDocument pdfDoc = new PdfDocument();
+
+			//Intialize the ExcelToPdfConverter Settings
+			ExcelToPdfConverterSettings settings = new ExcelToPdfConverterSettings();
+			settings.LayoutOptions = LayoutOptions.NoScaling;
+
+			//Assign the PdfDocument to the templateDocument property of ExcelToPdfConverterSettings
+			settings.TemplateDocument = pdfDoc;
+			settings.DisplayGridLines = GridLinesDisplayStyle.Invisible;
+
+			//Convert Excel Document into PDF document
+			pdfDoc = converter.Convert(settings);
+
+			//Save the PDF file
+			pdfDoc.Save(pdfstream);
+
+			//Load the document to pdfviewer
+			pdfviewer.Load(pdfstream);
+
+			//Show the print dialog.
+			pdfviewer.Print(true);
+
+		}
+		#endregion
+
+
+	}
+}
